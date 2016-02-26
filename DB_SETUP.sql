c145d3e7aeeee11e65b00ef8742e5fe98ce214b7
stevenrau <srau14@gmail.com>
2016-02-25 21:13:42 -0600

    Completed SQL db setup script
    -> Made script to create the music DB and build the three
       tables: artists, albums, and tracks
    -> THe setup script also adds two default artists to the artists
       table
    -> Also added gitignore file

--- DB_SETUP.sql
+++ DB_SETUP.sql
@@ -1,18 +1,52 @@
--- Create a database
-CREATE DATABASE dbli
+-- Create and select the database
+CREATE DATABASE IF NOT EXISTS music ;
+USE music ;
 
 
--- Create a Table
-CREATE TABLE TEST(
-	id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	firstname VARCHAR(30) NOT NULL,
-	lastname VARCHAR(30) NOT NULL,
-	email VARCHAR(30) NOT NULL,
-	reg_date TIMESTAMP
-)
+-- Create the three tables
+
+CREATE TABLE IF NOT EXISTS artists
+(
+  id             int(6)       UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name           varchar(50)  NOT NULL,
+  thumbnail_url  varchar(256)
+           -- Artist thumnbnail img URL can be empty since we want to allow creating an
+           -- artist without providing a thumbnail image.
+           -- If thumbnail image is provided for an album, it will be stored in
+           -- a file folder and the location will be saved here
+);
+
+CREATE TABLE IF NOT EXISTS albums
+(
+  id           int(6)         UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title        varchar(50)    NOT NULL,
+  artist_id    int(6)         UNSIGNED NOT NULL,
+  artwork_url  varchar(256),
+           -- Artwork URL can be empty since we want to allow creating an
+           -- album without providing artwork
+           -- If atwork is provided for an album, it will be stored in a file
+           -- folder and the location will be saved here
+  FOREIGN KEY (artist_id)  REFERENCES artists(id) ON DELETE CASCADE
+           -- Cascade DELETE from parent table 'artists' to this child table
+           -- i.e. If an artist is deleted from the artist table, all records
+           --      involving that atist in this child table are also deleted
+);
+
+CREATE TABLE IF NOT EXISTS tracks
+(
+  id         int(6)       UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title      varchar(50)  NOT NULL,
+  artist_id  int(6)       UNSIGNED NOT NULL,
+  album_id   int(6)       UNSIGNED NOT NULL,
+  FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE CASCADE,
+           -- Cascade DELETE from parent table 'artists' to this child table
+           -- i.e. If an artist is deleted from the artist table, all records
+           --      involving that atist in this child table are also deleted
+  FOREIGN KEY (album_id)  REFERENCES albums(id) ON DELETE CASCADE
+);
+
 
 -- Insert into a table
- INSERT INTO posts(firstname, lastname,email,comment) VALUES ('Mayra','Samaniego','mas686@mayra.com','Comment nm')
+INSERT INTO artists(name) VALUES ('John Denver');
+INSERT INTO artists(name) VALUES ('deadmau5');
 
--- Update a fiel of a row in a table
-UPDATE posts SET firstname="Lorena" WHERE id=1
