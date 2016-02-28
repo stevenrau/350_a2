12779273243f062e08973b2c763424964f51e7ac
stevenrau <srau14@gmail.com>
2016-02-28 14:44:16 -0600

    Updated SQL script to add albums
    -> SQL setup script now adds some default albums
    -> Added default albums artwork

--- DB_SETUP.sql
+++ DB_SETUP.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS tracks
   id         int(6)       UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   title      varchar(50)  NOT NULL,
   artist_id  int(6)       UNSIGNED NOT NULL,
-  album_id   int(6)       UNSIGNED,
+  album_id   int(6)       UNSIGNED NOT NULL,
   FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE CASCADE ON UPDATE CASCADE,
            -- Cascade DELETE from parent table 'artists' to this child table
            -- i.e. If an artist is deleted from the artist table, all records
@@ -46,9 +46,9 @@ CREATE TABLE IF NOT EXISTS tracks
 );
 
 
--- Insert into a table
+-- Insert artists into a table
 INSERT INTO artists(name) VALUES ('John Denver');
-INSERT INTO artists(name) VALUES ('deadmau5');
+INSERT INTO artists(name) VALUES ('Kendrick Lamar');
 INSERT INTO artists(name) VALUES ('Adele');
 INSERT INTO artists(name) VALUES ('Taylor Swift');
 INSERT INTO artists(name) VALUES ('Johnny Cash');
@@ -57,3 +57,13 @@ INSERT INTO artists(name) VALUES ('Johnny Cash');
 UPDATE artists SET thumbnail_url='/350_a2/artist_thumbnail/John Denver.jpg' WHERE name='John Denver';
 UPDATE artists SET thumbnail_url='/350_a2/artist_thumbnail/Adele.jpg' WHERE name='Adele';
 UPDATE artists SET thumbnail_url='/350_a2/artist_thumbnail/Taylor Swift.jpg' WHERE name='Taylor Swift';
+
+
+-- Insert albums into a table
+INSERT INTO albums(title, artist_id) VALUES ('To Pimp a Butterfly', 2);
+INSERT INTO albums(title, artist_id) VALUES ('25', 3);
+INSERT INTO albums(title, artist_id) VALUES ('1989', 4);
+
+-- Give some of the albums artwork
+UPDATE albums SET artwork_url='/350_a2/artwork/To Pimp a Butterfly.png' WHERE title='To Pimp a Butterfly';
+UPDATE albums SET artwork_url='/350_a2/artwork/1989.jpg' WHERE title='1989';
